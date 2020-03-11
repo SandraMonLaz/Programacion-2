3e3aa0665235c2787d65edc978a2eda50d27c611
PabloVillapunMartin <pvillapu@ucm.es>
2020-03-11 19:21:01 +0100

    Revert "Revert "P2""
    This reverts commit 5935e8479707600619941310d7a8750c2fca8103.

---
+++ P2/SDLProject/TPV2/AsteroidLifetime.cpp
@@ -0,0 +1,1 @@
+#include "AsteroidLifetime.h"
---
+++ P2/SDLProject/TPV2/GameCtrlSystem.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "ecs.h"
+#include "System.h"
+#include "Score.h"
+#include "GameState.h"
+#include "Entity.h"
+#include "Health.h"
+
+class GameCtrlSystem: public System {
+public:
+
+	GameCtrlSystem() : System(ecs::_sys_GameCtrl) {}
+
+	void init() override;
+	void update() override;
+};
+
